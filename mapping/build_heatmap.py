8dfea0c9e34c5e8d63ec3609861606d54ec4eb5e
antoinedang <antoine.dangeard@gmail.com>
2023-03-11 01:10:17 -0500

    Update build_heatmap.py

--- mapping/build_heatmap.py
+++ mapping/build_heatmap.py
@@ -1,3 +1,7 @@
+import numpy as np
+import math
+import pickle
+import cv2
 
 horizontal_fov = 66 #degrees
 aspect_ratio = 1200/1600
@@ -7,28 +11,68 @@ pool_floor_height = 0 #assume pool is on the ground
 
 visual_map_precision = 0.01 #in m
 green_reduction_intensity = int(0.2*255) #out of 255
-visual_map = np.zeros((int(pool_dimensions[0]/visual_map_precision), int(pool_dimensions[1]/visual_map_precision), 3))
+visual_map = np.zeros((int(pool_dimensions[0]/visual_map_precision), int(pool_dimensions[1]/visual_map_precision), 5))
+assumeRobotAlwaysCentered = True
 
-def save_map_to_file():
-    with open("map.pickle") as f:
-        pickle.dump(imgs[feed_name], f)
+start_y = None
+start_x = None
+all_imgs = []
+
+def save_to_file(data, name):
+    with open(name, "wb") as f:
+        pickle.dump(data, f)
+
+def load_from_file(name):
+    with open(name, "rb") as f:
+        return pickle.load(f)
 
 def remove_green_tint(img):
     img_b, img_g, img_r = cv2.split(img) #split by channel
     img_g = np.uint16(img_g)
-    img_g += color_shift_intensity
+    img_g -= color_shift_intensity
     np.clip(img_g, 0, 255, out=img_g)
     img_g = np.uint8(img_g)
     img = cv2.merge((img_b, img_g, img_r)) #merge adjusted channels
     return img
 
-def roundToPrecision():
-    decimals = math.log10()
+def roundToPrecision(x):
+    decimals = math.log10(visual_map_precision)
+    return int(x*(10**decimals))/(10**decimals)
 
-def add_mapping(color, world_x, world_y, x_distance_from_center, y_distance_from_center):
-    
+def add_mapping(color, world_x, world_y, x_conf, y_conf):
+    global visual_map
+    x_coord = roundToPrecision(world_x)-start_x
+    y_coord = roundToPrecision(world_y)-start_y
+    if x_coord < 0 or x_coord > visual_map.shape[0]: return
+    if y_coord < 0 or y_coord > visual_map.shape[1]: return
+    #NOT SURE WHICH IS BEST TAKE EITHER OF IMPLEMENTATIONS BELOW
+    #WEIGHTED AVERAGE
+    total_conf = (x_conf + visual_map[x_coord][y_coord][3] + y_conf + visual_map[x_coord][y_coord][4])
+    new_color_conf = (x_conf + y_conf)
+    current_color_conf = (visual_map[x_coord][y_coord][3] + visual_map[x_coord][y_coord][4])
+    visual_map[x_coord][y_coord][0] = (color[0]*new_color_conf + visual_map[x_coord][y_coord][0]*current_color_conf)/total_conf
+    visual_map[x_coord][y_coord][1] = (color[1]*new_color_conf + visual_map[x_coord][y_coord][1]*current_color_conf)/total_conf
+    visual_map[x_coord][y_coord][2] = (color[2]*new_color_conf + visual_map[x_coord][y_coord][2]*current_color_conf)/total_conf
+    visual_map[x_coord][y_coord][3] = (x_conf + visual_map[x_coord][y_coord][3])/2
+    visual_map[x_coord][y_coord][4] = (y_conf + visual_map[x_coord][y_coord][4])/2
 
-def image_cb(img, x, y, z):
+    #HIGHEST CONFIDENCE WITH PRIORITY ON Y
+    if (visual_map[x_coord][y_coord][4] < y_conf):
+        visual_map[x_coord][y_coord][0] = color[0]
+        visual_map[x_coord][y_coord][1] = color[1]
+        visual_map[x_coord][y_coord][2] = color[2]
+        visual_map[x_coord][y_coord][3] = x_conf
+        visual_map[x_coord][y_coord][4] = y_conf
+    elif (visual_map[x_coord][y_coord][4] == y_conf and visual_map[x_coord][y_coord][3] < x_conf)
+        visual_map[x_coord][y_coord][0] = color[0]
+        visual_map[x_coord][y_coord][1] = color[1]
+        visual_map[x_coord][y_coord][2] = color[2]
+        visual_map[x_coord][y_coord][3] = x_conf
+        visual_map[x_coord][y_coord][4] = y_conf
+
+def add_img_to_map(data):
+    img, x, y, z = data
+    if assumeRobotAlwaysCentered: x = start_x
     height, width, _ = img.shape
     for r in range(len(img)):
         row = img[r]
@@ -44,3 +88,33 @@ def image_cb(img, x, y, z):
             pixel = row[p]
             world_x = x + x_center_amt*depth*horizontal_fov
             add_mapping(pixel, world_x, world_y, x_confidence, y_confidence)
+
+def image_cb(img, x, y, z):
+    global all_imgs
+    global start_x
+    global start_y
+    if start_x == None: start_x = x
+    if start_y == None: start_y = y
+    #ADD LIVE FEED HERE?
+    img = remove_green_tint(img)
+    all_imgs.append((img,x,y,z))
+    save_to_file(all_imgs, "image_data.sav")
+
+def build_heatmap():
+    #make grey black
+    #make white true white
+    #make yellow pure yellow
+    #etc
+    #indicate center points of dead zones (white zones)
+    return
+
+def liveFeed():
+    while True:
+        img_data = load_from_file("image_data.sav")
+        cv2.imshow("live", img_data[-1][0])
+
+if __name__ == "__main__":
+    img_data = load_from_file("image_data.sav")
+    for data in img_data:
+        add_img_to_map(data)
+    build_heatmap()
